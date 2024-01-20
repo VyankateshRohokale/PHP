a096719b43d72a4400edbfb3fe4f8d990173ca03
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2024-01-20 10:43:42 +0530

    Create 1stProgram.php

---
+++ 1stProgram.php
@@ -0,0 +1,26 @@
+// this the first code of pho where we print something kn console by using echo...
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>
+    My Web Page
+  </title>
+</head>
+
+  <body>
+
+<?php
+echo "Jay Ganesh";
+echo "Jay Shree Krishna";
+
+
+?>
+    
+  </body>
+
+
+
+  
+</html>
+
