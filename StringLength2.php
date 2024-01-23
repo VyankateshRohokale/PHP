eec75284affdd1be77deb0b14d6d6930195de91d
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2024-01-23 19:54:17 +0530

    Create StringLength2.php

---
+++ StringLength2.php
@@ -0,0 +1,15 @@
+/* We used inbuild function for finding the length of the string, 
+to perform this same output without using Inbuild function checkout , 
+StringLength.php  
+*/
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo strlen("Hello world!");
+?> 
+ 
+</body>
+</html>
