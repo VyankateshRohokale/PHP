385775ed1f6e98c7bb4076840f3c22909f258828
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2024-01-24 19:32:39 +0530

    Create ReplaceString.php

---
+++ ReplaceString.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+
+  <body>
+
+<?php
+$name = "Hello World!";
+echo("hello Piyush !! <br>");
+echo str_replace("Piyush", "Vyankatesh", $name);
+
+?> 
+ 
+
+</body>
+
+ </html>
