c65f1a1898eee65499d4ae5ab669c24fe334e882
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2024-02-04 12:10:43 +0530

    Create CastingStrings&FloatstoInt.php

---
+++ CastingStrings&FloatstoInt.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+ 
+$no = 123465.768;
+$int_cast = (int)$no;
+echo $int_cast;
+  
+echo "<br>";
+
+$no = "123465.768";
+$int_cast = (int)$no;
+echo $int_cast;
+?>  
+
+</body>
+</html>
