c35a32c8b36caa5971e47f575d08232a3a19b359
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2024-02-04 12:01:45 +0530

    Create Numbers.php

---
+++ Numbers.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$a = 11;
+
+$b = 11.11;
+
+$c = "11";
+
+var_dump($a);
+
+echo "<br>";
+
+var_dump($b);
+echo "<br>";
+
+var_dump($c);
+
+?> 
+
+
+
+</body>
+</html>
