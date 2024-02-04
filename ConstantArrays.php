7c0bf7dbd39ed65678d880473c789037e4dd1536
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2024-02-04 12:59:42 +0530

    Create ConstantArrays.php

---
+++ ConstantArrays.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+define("names", ["Vyankatesh" , "Piyush" , "Chiki"]);
+echo names[0];
+echo "<br>";
+echo names[1];
+echo "<br>";
+echo names[2];
+?> 
+
+</body>
+</html>
