c1cf576b9c13123df70e27046cc312beef90068d
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2024-02-05 23:19:06 +0530

    Create array1.php

---
+++ array1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+
+
+
+$Arr = array("Piyush", 21,"Vyankatesh");
+
+echo "$Arr[0] <br> ";
+echo "$Arr[1] <br> ";
+echo "$Arr[2] <br> ";
+?>  
+
+</body>
+</html>
