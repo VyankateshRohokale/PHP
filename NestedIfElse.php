4f212f0bfda8a5b8dab86fc5592e834c20d84329
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2024-02-16 12:33:15 +0530

    Create NestedIfElse.php

---
+++ NestedIfElse.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<body>
+
+<?php
+$no = 51;
+
+if($no > 21) 
+{
+  echo"$no is greater than 21<br>";
+  if ($no > 20) 
+  {
+    echo "It is aso greater than 20<br>";
+  } 
+  else 
+  {
+    echo "Its not greater than 20<br>";
+  }
+}
+?>
+
+	</body>
+</html>
