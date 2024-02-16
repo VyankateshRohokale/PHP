28285ac4854cf4e7b3dd2733d4ee5dc323bee4ba
Vyankatesh Rohokale <138516458+VyankateshRohokale@users.noreply.github.com>
2024-02-16 12:36:52 +0530

    Create SwitchCase.php

---
+++ SwitchCase.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+ <body>
+
+<?php
+$no = 51;
+
+switch ($no) 
+{
+  case 11:
+    echo "you entered 11";
+    break;
+  case 21:
+    echo "you entered 21";
+    break;
+  case 51:
+    echo "you entered 51";
+    break;
+  default:
+    echo "Its not 11 ,21 and 51";
+}
+?>
+ 
+ </body>
+</html>
